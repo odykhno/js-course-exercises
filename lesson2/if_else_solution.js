525df4a5dd3c9138ab353e55d703a4280643f96b
Olena Dykhno <olenadykhno@gmail.com>
2016-03-20 14:31:51 +0200

    finished if-else part

---
+++ lesson2/if_else_solution.js
@@ -0,0 +1,35 @@
+﻿function greaterNum(a,b) {
+if (a>b) return a;
+else return b;
+}
+
+function helloWorld(lang) {
+switch (lang) {
+case 'en':
+  return'Hello';
+  break;
+case 'ua':
+  return'Привіт';
+  break;
+case 'es':
+  return'Hola';
+  break;
+default:
+  return'Привет';
+}
+}
+
+function assignGrade(ball) {
+     if (ball >=1 && ball <=20) {
+     return 1;
+      } else if (ball <= 40) {
+      return 2;
+       } else if (ball <= 60) {
+         return 3;
+         } else if (ball <= 80) {
+           return 4;
+            } else {
+              return 5;
+      }
+     }
+
