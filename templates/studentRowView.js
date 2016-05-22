bd2f0129f945b243c3a457220c0cb584420b7da8
Anton Tkachov <lookformeon@gmail.com>
2016-05-22 18:23:51 +0300

    Add Mustache and lodash to index.html. Implement studentRowView template

---
+++ templates/studentRowView.js
@@ -0,0 +1,11 @@
+var STUDENT_ROW_VIEW = `
+<tr>
+  <td>{{first_name}}</td>
+  <td>{{last_name}}</td>
+  <td data-id={{id}}>
+    <a href="#" class="btn btn-default">Show</a>
+    <a href="#" class="btn btn-primary">Edit</a>
+    <a href="#" class="btn btn-danger">Delete</a>
+  </td>
+</tr>
+`
